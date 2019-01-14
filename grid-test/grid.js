ecb9dfb9ec572b4ca8929b7dfe4498d4c04bd125
John Doe <patrick.gould94@gmail.com>
2019-01-14 16:58:40 -0500

    commiting for checkout

--- grid-test/grid.js
+++ grid-test/grid.js
@@ -3,52 +3,56 @@ let columns = 5;
 let rows = 5;
 
 const tile = {
-  x: 30,
-  y: 30
+    x: 30,
+    y: 30
 };
 
 const canvas = {
-  height: 605,
-  width: 605
+    height: 605,
+    width: 605
 };
 
 const grid = {
-  columnsAmount: columns,
-  rowsAmount: rows
+    columnsAmount: columns,
+    rowsAmount: rows
 };
 
 
 
 function setup() {
-  createCanvas(canvas.width, canvas.height);
+    createCanvas(canvas.width, canvas.height);
   
 }
 
 function draw() {
-  background(51);
+    background(51);
 
-  for(let i = 0; i < grid.columnsAmount; i++) {
-    for(let n = 0; n < grid.rowsAmount; n++) {
-      let tileWidth = (canvas.width - 5) / grid.columnsAmount;
-      let tileHeight = (canvas.height - 5) / grid.rowsAmount;
-      tile.x = i * tileWidth + 2;
-      tile.y = n * tileHeight + 2;
-      stroke(0);
-      fill(255);
-      rect(tile.x, tile.y, tileWidth, tileHeight);
+    for(let i = 0; i < grid.columnsAmount; i++) {
+        for(let n = 0; n < grid.rowsAmount; n++) {
+            let tileWidth = (canvas.width - 5) / grid.columnsAmount;
+            let tileHeight = (canvas.height - 5) / grid.rowsAmount;
+            tile.x = i * tileWidth + 2;
+            tile.y = n * tileHeight + 2;
+
+            stroke(0);
+            fill(255);
+            rect(tile.x, tile.y, tileWidth, tileHeight);
+            fill(0,0,255);
+            text("1", tile.x + 60, tile.y + 60);
+            
+        }
     }
-  }
 }
 
 $(function() {
 
-  $("#column-select").on("change", function() {
-    grid.columnsAmount = $("#column-select").val();
+    $("#column-select").on("change", function() {
+        grid.columnsAmount = $("#column-select").val();
     // draw();
-  });
+    });
 
-  $("#row-select").on("change", function() {
-    grid.rowsAmount = $("#row-select").val();
+    $("#row-select").on("change", function() {
+        grid.rowsAmount = $("#row-select").val();
     // draw();
-  });
+    });
 });
